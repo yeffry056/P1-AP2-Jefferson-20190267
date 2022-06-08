d3e4e5579e4739b48b283a0a97c6db80771333af
Jefferson <89818536+yeffry056@users.noreply.github.com>
2022-06-08 19:40:24 -0400

    Creacion de registro

--- app/src/main/java/com/jjvcorporation/p1_ap2_jefferson_20190267/model/Prestamo.kt
+++ app/src/main/java/com/jjvcorporation/p1_ap2_jefferson_20190267/model/Prestamo.kt
@@ -1,9 +1,11 @@
 package com.jjvcorporation.p1_ap2_jefferson_20190267.model
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "Prestamos")
 data class Prestamo(
+    @PrimaryKey(autoGenerate = true)
     val prestamoId: Int =0,
     val deudor: String,
     val concepto : String,
